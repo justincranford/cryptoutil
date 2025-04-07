baa848b5647ae888a6152b9187f2965e8ca70bfa
Justin Cranford <justincranford@hotmail.com>
2025-04-07 15:48:18 -0400

    Split pool tests into finite vs indefinite

--- internal/crypto/keygen/keypool_test.go
+++ internal/crypto/keygen/keypool_test.go
@@ -15,15 +15,17 @@ import (
 	cryptoutilTelemetry "cryptoutil/internal/telemetry"
 
 	googleUuid "github.com/google/uuid"
+
 	"github.com/stretchr/testify/assert"
 )
 
-const (
-	testNumWorkers = 2
-	testSize       = 3
-	testMaxSize    = 3
-	testMaxTime    = 3 * time.Second
-)
+type TestCase struct {
+	name           string
+	testNumWorkers int
+	testSize       int
+	testMaxSize    int
+	testMaxTime    time.Duration
+}
 
 var (
 	testCtx              = context.Background()
@@ -42,78 +44,134 @@ func TestMain(m *testing.M) {
 }
 
 func TestPoolRSA(t *testing.T) {
-	pool := NewKeyPool(testCtx, testTelemetryService, "Test RSA 2048", testNumWorkers, testSize, testMaxSize, testMaxTime, GenerateRSAKeyPairFunction(2048))
-	defer pool.Close()
+	testCases := []TestCase{
+		{name: "Finite RSA 2048", testNumWorkers: 2, testSize: 3, testMaxSize: 3, testMaxTime: 3 * time.Second},
+		{name: "Indefinite RSA 2048", testNumWorkers: 2, testSize: 3, testMaxSize: MaxKeys, testMaxTime: MaxTime},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			pool := NewKeyPool(testCtx, testTelemetryService, tc.name, tc.testNumWorkers, tc.testSize, tc.testMaxSize, tc.testMaxTime, GenerateRSAKeyPairFunction(2048))
+			defer pool.Close()
 
-	for i := 0; i < testMaxSize; i++ {
-		keyPair := pool.Get()
-		assert.IsType(t, &rsa.PrivateKey{}, keyPair.Private)
-		assert.IsType(t, &rsa.PublicKey{}, keyPair.Public)
+			for i := 0; i < tc.testMaxSize; i++ {
+				keyPair := pool.Get()
+				assert.IsType(t, &rsa.PrivateKey{}, keyPair.Private)
+				assert.IsType(t, &rsa.PublicKey{}, keyPair.Public)
+			}
+		})
 	}
 }
 
 func TestPoolEcDSA(t *testing.T) {
-	pool := NewKeyPool(testCtx, testTelemetryService, "Test ECDSA P256", testNumWorkers, testSize, testMaxSize, testMaxTime, GenerateECDSAKeyPairFunction(elliptic.P256()))
-	defer pool.Close()
+	testCases := []TestCase{
+		{name: "Finite ECDSA P256", testNumWorkers: 2, testSize: 3, testMaxSize: 3, testMaxTime: 3 * time.Second},
+		{name: "Indefinite ECDSA P256", testNumWorkers: 2, testSize: 3, testMaxSize: MaxKeys, testMaxTime: MaxTime},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			pool := NewKeyPool(testCtx, testTelemetryService, tc.name, tc.testNumWorkers, tc.testSize, tc.testMaxSize, tc.testMaxTime, GenerateECDSAKeyPairFunction(elliptic.P256()))
+			defer pool.Close()
 
-	for i := 0; i < testMaxSize; i++ {
-		keyPair := pool.Get()
-		assert.IsType(t, &ecdsa.PrivateKey{}, keyPair.Private)
-		assert.IsType(t, &ecdsa.PublicKey{}, keyPair.Public)
+			for i := 0; i < tc.testMaxSize; i++ {
+				keyPair := pool.Get()
+				assert.IsType(t, &ecdsa.PrivateKey{}, keyPair.Private)
+				assert.IsType(t, &ecdsa.PublicKey{}, keyPair.Public)
+			}
+		})
 	}
 }
 
 func TestPoolEcDH(t *testing.T) {
-	pool := NewKeyPool(testCtx, testTelemetryService, "Test ECDH P256", testNumWorkers, testSize, testMaxSize, testMaxTime, GenerateECDHKeyPairFunction(ecdh.P256()))
-	defer pool.Close()
+	testCases := []TestCase{
+		{name: "Finite ECDH P256", testNumWorkers: 2, testSize: 3, testMaxSize: 3, testMaxTime: 3 * time.Second},
+		{name: "Indefinite ECDH P256", testNumWorkers: 2, testSize: 3, testMaxSize: MaxKeys, testMaxTime: MaxTime},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			pool := NewKeyPool(testCtx, testTelemetryService, tc.name, tc.testNumWorkers, tc.testSize, tc.testMaxSize, tc.testMaxTime, GenerateECDHKeyPairFunction(ecdh.P256()))
+			defer pool.Close()
 
-	for i := 0; i < testMaxSize; i++ {
-		keyPair := pool.Get()
-		assert.IsType(t, &ecdh.PrivateKey{}, keyPair.Private)
-		assert.IsType(t, &ecdh.PublicKey{}, keyPair.Public)
+			for i := 0; i < tc.testMaxSize; i++ {
+				keyPair := pool.Get()
+				assert.IsType(t, &ecdh.PrivateKey{}, keyPair.Private)
+				assert.IsType(t, &ecdh.PublicKey{}, keyPair.Public)
+			}
+		})
 	}
 }
 
 func TestPoolEdDSA(t *testing.T) {
-	pool := NewKeyPool(testCtx, testTelemetryService, "Test Ed25519", testNumWorkers, testSize, testMaxSize, testMaxTime, GenerateEDKeyPairFunction("Ed25519"))
-	defer pool.Close()
+	testCases := []TestCase{
+		{name: "Finite Ed25519", testNumWorkers: 2, testSize: 3, testMaxSize: 3, testMaxTime: 3 * time.Second},
+		{name: "Indefinite Ed25519", testNumWorkers: 2, testSize: 3, testMaxSize: MaxKeys, testMaxTime: MaxTime},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			pool := NewKeyPool(testCtx, testTelemetryService, tc.name, tc.testNumWorkers, tc.testSize, tc.testMaxSize, tc.testMaxTime, GenerateEDKeyPairFunction("Ed25519"))
+			defer pool.Close()
 
-	for i := 0; i < testMaxSize; i++ {
-		keyPair := pool.Get()
-		assert.IsType(t, ed25519.PrivateKey{}, keyPair.Private)
-		assert.IsType(t, ed25519.PublicKey{}, keyPair.Public)
+			for i := 0; i < tc.testMaxSize; i++ {
+				keyPair := pool.Get()
+				assert.IsType(t, ed25519.PrivateKey{}, keyPair.Private)
+				assert.IsType(t, ed25519.PublicKey{}, keyPair.Public)
+			}
+		})
 	}
 }
 
 func TestPoolAES(t *testing.T) {
-	pool := NewKeyPool(testCtx, testTelemetryService, "Test AES 128", testNumWorkers, testSize, testMaxSize, testMaxTime, GenerateAESKeyFunction(128))
-	defer pool.Close()
+	testCases := []TestCase{
+		{name: "Finite AES 128", testNumWorkers: 2, testSize: 3, testMaxSize: 3, testMaxTime: 3 * time.Second},
+		{name: "Indefinite AES 128", testNumWorkers: 2, testSize: 3, testMaxSize: MaxKeys, testMaxTime: MaxTime},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			pool := NewKeyPool(testCtx, testTelemetryService, tc.name, tc.testNumWorkers, tc.testSize, tc.testMaxSize, tc.testMaxTime, GenerateAESKeyFunction(128))
+			defer pool.Close()
 
-	for i := 0; i < testMaxSize; i++ {
-		keyPair := pool.Get()
-		assert.IsType(t, []byte{}, keyPair.Private)
-		assert.Nil(t, keyPair.Public)
+			for i := 0; i < tc.testMaxSize; i++ {
+				keyPair := pool.Get()
+				assert.IsType(t, []byte{}, keyPair.Private)
+				assert.Nil(t, keyPair.Public)
+			}
+		})
 	}
 }
 
 func TestPoolHMAC(t *testing.T) {
-	pool := NewKeyPool(testCtx, testTelemetryService, "Test HMAC 256", testNumWorkers, testSize, testMaxSize, testMaxTime, GenerateHMACKeyFunction(256))
-	defer pool.Close()
+	testCases := []TestCase{
+		{name: "Finite HMAC 256", testNumWorkers: 2, testSize: 3, testMaxSize: 3, testMaxTime: 3 * time.Second},
+		{name: "Indefinite HMAC 256", testNumWorkers: 2, testSize: 3, testMaxSize: MaxKeys, testMaxTime: MaxTime},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			pool := NewKeyPool(testCtx, testTelemetryService, tc.name, tc.testNumWorkers, tc.testSize, tc.testMaxSize, tc.testMaxTime, GenerateHMACKeyFunction(256))
+			defer pool.Close()
 
-	for i := 0; i < testMaxSize; i++ {
-		keyPair := pool.Get()
-		assert.IsType(t, []byte{}, keyPair.Private)
-		assert.Nil(t, keyPair.Public)
+			for i := 0; i < tc.testMaxSize; i++ {
+				keyPair := pool.Get()
+				assert.IsType(t, []byte{}, keyPair.Private)
+				assert.Nil(t, keyPair.Public)
+			}
+		})
 	}
 }
 
 func TestPoolUUIDv7(t *testing.T) {
-	pool := NewKeyPool(testCtx, testTelemetryService, "Test UUID V7", testNumWorkers, testSize, testMaxSize, testMaxTime, GenerateUUIDv7Function())
-	defer pool.Close()
+	testCases := []TestCase{
+		{name: "Finite UUID V7", testNumWorkers: 2, testSize: 3, testMaxSize: 3, testMaxTime: 3 * time.Second},
+		{name: "Indefinite UUID V7", testNumWorkers: 2, testSize: 3, testMaxSize: MaxKeys, testMaxTime: MaxTime},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			pool := NewKeyPool(testCtx, testTelemetryService, tc.name, tc.testNumWorkers, tc.testSize, tc.testMaxSize, tc.testMaxTime, GenerateUUIDv7Function())
+			defer pool.Close()
 
-	for i := 0; i < testMaxSize; i++ {
-		keyPair := pool.Get()
-		assert.IsType(t, googleUuid.UUID{}, keyPair.Private)
-		assert.Nil(t, keyPair.Public)
+			for i := 0; i < tc.testMaxSize; i++ {
+				keyPair := pool.Get()
+				assert.IsType(t, googleUuid.UUID{}, keyPair.Private)
+				assert.Nil(t, keyPair.Public)
+			}
+		})
 	}
 }
