57d5c30696c28aabd7fe0254a71c7c43d38c1439
Justin Cranford <justincranford@hotmail.com>
2025-04-09 11:06:31 -0400

    Added cached getters util class

---
+++ internal/util/cached_getters.go
@@ -0,0 +1,32 @@
+package util
+
+import "sync"
+
+func GetCached(cached bool, syncOnce *sync.Once, getterFunction func() any) any {
+	var value any
+	if cached {
+		syncOnce.Do(func() {
+			value = getterFunction()
+		})
+		return value
+	}
+	syncOnce.Do(func() {
+		value = getterFunction()
+	})
+	return value
+}
+
+func GetCachedWithError(cached bool, syncOnce *sync.Once, getterFunction func() (any, error)) (any, error) {
+	var value any
+	var err error
+	if cached {
+		syncOnce.Do(func() {
+			value, err = getterFunction()
+		})
+		return value, err
+	}
+	syncOnce.Do(func() {
+		value, err = getterFunction()
+	})
+	return value, err
+}
