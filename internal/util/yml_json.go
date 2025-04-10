0f371448214bdbe01b13e9fc600ece83df8e6b2b
Justin Cranford <justincranford@hotmail.com>
2025-04-10 09:59:55 -0400

    Added convert utils for yaml<=>json

---
+++ internal/util/yml_json.go
@@ -0,0 +1,58 @@
+package util
+
+import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/goccy/go-yaml"
+)
+
+func YAML2JSON(y string) (string, error) {
+	object, err := ParseYAML(y)
+	if err != nil {
+		return "", err
+	}
+
+	return EncodeJSON(object)
+}
+
+func JSON2YAML(j string) (string, error) {
+	object, err := ParseJSON(j)
+	if err != nil {
+		return "", err
+	}
+
+	return EncodeYAML(object)
+}
+
+func ParseYAML(y string) (any, error) {
+	var object any
+	if err := yaml.Unmarshal([]byte(y), &object); err != nil {
+		return nil, fmt.Errorf("failed to parse YAML: %w", err)
+	}
+	return object, nil
+}
+
+func ParseJSON(j string) (any, error) {
+	var object any
+	if err := json.Unmarshal([]byte(j), &object); err != nil {
+		return nil, fmt.Errorf("failed to parse JSON: %w", err)
+	}
+	return object, nil
+}
+
+func EncodeYAML(object any) (string, error) {
+	yamlContent, err := yaml.Marshal(object)
+	if err != nil {
+		return "", fmt.Errorf("failed to encode YAML: %w", err)
+	}
+	return string(yamlContent), nil
+}
+
+func EncodeJSON(object any) (string, error) {
+	jsonContent, err := json.Marshal(object)
+	if err != nil {
+		return "", fmt.Errorf("failed to encode JSON: %w", err)
+	}
+	return string(jsonContent), nil
+}
---
+++ internal/util/yml_json_test.go
@@ -0,0 +1,90 @@
+package util
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	validYAML       = "key1: value1\nkey2: value2\n"
+	invalidYAML     = "key1 value1\nkey2: value2\n"
+	validJSON       = `{"key1":"value1","key2":"value2"}`
+	invalidJSON     = `{"key1":"value1", "key2":}`
+	parsedObject    = map[string]interface{}{"key1": "value1", "key2": "value2"}
+	singleParsedObj = map[string]interface{}{"key": "value"}
+	validYAMLSingle = "key: value\n"
+	validJSONSingle = `{"key":"value"}`
+)
+
+func TestHappyPathYAML2JSON(t *testing.T) {
+	result, err := YAML2JSON(validYAML)
+	require.NoError(t, err)
+	require.Equal(t, validJSON, result)
+}
+
+func TestSadPathYAML2JSON(t *testing.T) {
+	_, err := YAML2JSON(invalidYAML)
+	require.Error(t, err)
+	require.EqualError(t, err, "failed to parse YAML: [1:1] unexpected key name\n>  1 | key1 value1\n   2 | key2: value2\n       ^\n")
+}
+
+func TestHappyPathJSON2YAML(t *testing.T) {
+	result, err := JSON2YAML(validJSON)
+	require.NoError(t, err)
+	require.Equal(t, validYAML, result)
+}
+
+func TestSadPathJSON2YAML(t *testing.T) {
+	_, err := JSON2YAML(invalidJSON)
+	require.Error(t, err)
+	require.EqualError(t, err, "failed to parse JSON: invalid character '}' looking for beginning of value")
+}
+
+func TestHappyPathParseYAML(t *testing.T) {
+	object, err := ParseYAML(validYAMLSingle)
+	require.NoError(t, err)
+	require.Equal(t, "value", object.(map[string]interface{})["key"])
+}
+
+func TestSadPathParseYAML(t *testing.T) {
+	_, err := ParseYAML(invalidYAML)
+	require.Error(t, err)
+	require.EqualError(t, err, "failed to parse YAML: [1:1] unexpected key name\n>  1 | key1 value1\n   2 | key2: value2\n       ^\n")
+}
+
+func TestHappyPathParseJSON(t *testing.T) {
+	object, err := ParseJSON(validJSONSingle)
+	require.NoError(t, err)
+	require.Equal(t, "value", object.(map[string]interface{})["key"])
+}
+
+func TestSadPathParseJSON(t *testing.T) {
+	_, err := ParseJSON(invalidJSON)
+	require.Error(t, err)
+	require.EqualError(t, err, "failed to parse JSON: invalid character '}' looking for beginning of value")
+}
+
+func TestHappyPathEncodeYAML(t *testing.T) {
+	result, err := EncodeYAML(singleParsedObj)
+	require.NoError(t, err)
+	require.Equal(t, validYAMLSingle, result)
+}
+
+func TestSadPathEncodeYAML(t *testing.T) {
+	_, err := EncodeYAML(func() {})
+	require.Error(t, err)
+	require.EqualError(t, err, "failed to encode YAML: unknown value type func()")
+}
+
+func TestHappyPathEncodeJSON(t *testing.T) {
+	result, err := EncodeJSON(singleParsedObj)
+	require.NoError(t, err)
+	require.Equal(t, validJSONSingle, result)
+}
+
+func TestSadPathEncodeJSON(t *testing.T) {
+	_, err := EncodeJSON(func() {})
+	require.Error(t, err)
+	require.EqualError(t, err, "failed to encode JSON: json: unsupported type: func()")
+}
