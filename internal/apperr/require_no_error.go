dcd3169ac3916e52cca418f54f9a66236ebd1162
Justin Cranford <justincranford@hotmail.com>
2025-04-12 16:47:28 -0400

    Added comment to RequireNoError

--- internal/apperr/require_no_error.go
+++
@@ -1,9 +0,0 @@
-package apperr
-
-import "fmt"
-
-func RequireNoError(err error, message string) {
-	if err != nil {
-		panic(fmt.Sprintf(message+": %v", err))
-	}
-}
---
+++ internal/apperr/require_no_error_test_util.go
@@ -0,0 +1,10 @@
+package apperr
+
+import "fmt"
+
+// RequireNoError If non-nil error, panic with value "%s: %v" using provided message and error
+func RequireNoError(err error, message string) {
+	if err != nil {
+		panic(fmt.Sprintf("%s: %v", message, err))
+	}
+}
