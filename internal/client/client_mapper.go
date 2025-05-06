a819026938f0f390939305127cfbfb854a0d5cbc
Justin Cranford <justincranford@hotmail.com>
2025-05-05 21:52:38 -0400

    Moved client helper functions

---
+++ internal/client/client_mapper.go
@@ -0,0 +1,162 @@
+package client
+
+import (
+	cryptoutilOpenapiClient "cryptoutil/internal/openapi/client"
+	cryptoutilOpenapiModel "cryptoutil/internal/openapi/model"
+	"errors"
+	"fmt"
+	"strings"
+	"testing"
+)
+
+func MapKeyPoolCreate(t *testing.T, name string, description string, algorithm string, provider string, exportAllowed bool, importAllowed bool, versioningAllowed bool) (*cryptoutilOpenapiClient.PostKeypoolJSONRequestBody, error) {
+	keyPoolName, err1 := MapKeyPoolName(name)
+	keyPoolDescription, err2 := MapKeyPoolDescription(description)
+	keyPoolAlgorithm, err3 := MapKeyPoolAlgorithm(algorithm)
+	keyPoolProvider, err4 := MapKeyPoolProvider(provider)
+	keyPoolKeyPoolExportAllowed := MapKeyPoolExportAllowed(exportAllowed)
+	keyPoolKeyPoolImportAllowed := MapKeyPoolImportAllowed(importAllowed)
+	keyPoolKeyPoolVersioningAllowed := MapKeyPoolVersioningAllowed(versioningAllowed)
+	if err1 != nil || err2 != nil || err3 != nil || err4 != nil {
+		t.Fatalf("failed to map key pool: %v", errors.Join(err1, err2, err3, err4))
+	}
+	return &cryptoutilOpenapiModel.KeyPoolCreate{
+		Name:              *keyPoolName,
+		Description:       *keyPoolDescription,
+		Provider:          keyPoolProvider,
+		Algorithm:         keyPoolAlgorithm,
+		ExportAllowed:     keyPoolKeyPoolExportAllowed,
+		ImportAllowed:     keyPoolKeyPoolImportAllowed,
+		VersioningAllowed: keyPoolKeyPoolVersioningAllowed,
+	}, nil
+}
+
+func MapKeyPoolName(name string) (*cryptoutilOpenapiModel.KeyPoolName, error) {
+	if err := ValidateString(name); err != nil {
+		return nil, fmt.Errorf("invalid key pool name: %w", err)
+	}
+	keyPoolName := cryptoutilOpenapiModel.KeyPoolName(name)
+	return &keyPoolName, nil
+}
+
+func MapKeyPoolDescription(description string) (*cryptoutilOpenapiModel.KeyPoolDescription, error) {
+	if err := ValidateString(description); err != nil {
+		return nil, fmt.Errorf("invalid key pool description: %w", err)
+	}
+	keyPoolDescription := cryptoutilOpenapiModel.KeyPoolDescription(description)
+	return &keyPoolDescription, nil
+}
+
+func MapKeyPoolAlgorithm(algorithm string) (*cryptoutilOpenapiModel.KeyPoolAlgorithm, error) {
+	if err := ValidateString(algorithm); err != nil {
+		return nil, fmt.Errorf("invalid key pool algorithm: %w", err)
+	}
+	var keyPoolAlgorithm cryptoutilOpenapiModel.KeyPoolAlgorithm
+	switch algorithm {
+	case string(cryptoutilOpenapiModel.A128CBCHS256A128GCMKW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A128CBCHS256A128GCMKW
+	case string(cryptoutilOpenapiModel.A128CBCHS256A128KW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A128CBCHS256A128KW
+	case string(cryptoutilOpenapiModel.A128CBCHS256A192GCMKW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A128CBCHS256A192GCMKW
+	case string(cryptoutilOpenapiModel.A128CBCHS256A192KW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A128CBCHS256A192KW
+	case string(cryptoutilOpenapiModel.A128CBCHS256A256GCMKW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A128CBCHS256A256GCMKW
+	case string(cryptoutilOpenapiModel.A128CBCHS256A256KW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A128CBCHS256A256KW
+	case string(cryptoutilOpenapiModel.A128CBCHS256dir):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A128CBCHS256dir
+	case string(cryptoutilOpenapiModel.A128GCMA128GCMKW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A128GCMA128GCMKW
+	case string(cryptoutilOpenapiModel.A128GCMA128KW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A128GCMA128KW
+	case string(cryptoutilOpenapiModel.A128GCMA192GCMKW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A128GCMA192GCMKW
+	case string(cryptoutilOpenapiModel.A128GCMA192KW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A128GCMA192KW
+	case string(cryptoutilOpenapiModel.A128GCMA256GCMKW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A128GCMA256GCMKW
+	case string(cryptoutilOpenapiModel.A128GCMA256KW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A128GCMA256KW
+	case string(cryptoutilOpenapiModel.A128GCMdir):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A128GCMdir
+	case string(cryptoutilOpenapiModel.A192CBCHS384A192GCMKW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A192CBCHS384A192GCMKW
+	case string(cryptoutilOpenapiModel.A192CBCHS384A192KW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A192CBCHS384A192KW
+	case string(cryptoutilOpenapiModel.A192CBCHS384A256GCMKW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A192CBCHS384A256GCMKW
+	case string(cryptoutilOpenapiModel.A192CBCHS384A256KW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A192CBCHS384A256KW
+	case string(cryptoutilOpenapiModel.A192CBCHS384dir):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A192CBCHS384dir
+	case string(cryptoutilOpenapiModel.A192GCMA192GCMKW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A192GCMA192GCMKW
+	case string(cryptoutilOpenapiModel.A192GCMA192KW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A192GCMA192KW
+	case string(cryptoutilOpenapiModel.A192GCMA256GCMKW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A192GCMA256GCMKW
+	case string(cryptoutilOpenapiModel.A192GCMA256KW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A192GCMA256KW
+	case string(cryptoutilOpenapiModel.A192GCMdir):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A192GCMdir
+	case string(cryptoutilOpenapiModel.A256CBCHS512A256GCMKW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A256CBCHS512A256GCMKW
+	case string(cryptoutilOpenapiModel.A256CBCHS512A256KW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A256CBCHS512A256KW
+	case string(cryptoutilOpenapiModel.A256CBCHS512dir):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A256CBCHS512dir
+	case string(cryptoutilOpenapiModel.A256GCMA256GCMKW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A256GCMA256GCMKW
+	case string(cryptoutilOpenapiModel.A256GCMA256KW):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A256GCMA256KW
+	case string(cryptoutilOpenapiModel.A256GCMdir):
+		keyPoolAlgorithm = cryptoutilOpenapiModel.A256GCMdir
+	default:
+		return nil, fmt.Errorf("invalid key pool algorithm: %s", algorithm)
+	}
+	return &keyPoolAlgorithm, nil
+}
+
+func MapKeyPoolProvider(provider string) (*cryptoutilOpenapiModel.KeyPoolProvider, error) {
+	if err := ValidateString(provider); err != nil {
+		return nil, fmt.Errorf("invalid key pool provider: %w", err)
+	}
+	var keyPoolProvider cryptoutilOpenapiModel.KeyPoolProvider
+	switch provider {
+	case string(cryptoutilOpenapiModel.Internal):
+		keyPoolProvider = cryptoutilOpenapiModel.Internal
+	default:
+		return nil, fmt.Errorf("invalid key pool provider: %s", provider)
+	}
+	return &keyPoolProvider, nil
+}
+
+func MapKeyPoolImportAllowed(importAllowed bool) *cryptoutilOpenapiModel.KeyPoolImportAllowed {
+	keyPoolKeyPoolImportAllowed := cryptoutilOpenapiModel.KeyPoolImportAllowed(importAllowed)
+	return &keyPoolKeyPoolImportAllowed
+}
+
+func MapKeyPoolExportAllowed(exportAllowed bool) *cryptoutilOpenapiModel.KeyPoolExportAllowed {
+	keyPoolKeyPoolExportAllowed := cryptoutilOpenapiModel.KeyPoolExportAllowed(exportAllowed)
+	return &keyPoolKeyPoolExportAllowed
+}
+
+func MapKeyPoolVersioningAllowed(versioningAllowed bool) *cryptoutilOpenapiModel.KeyPoolVersioningAllowed {
+	keyPoolKeyPoolVersioningAllowed := cryptoutilOpenapiModel.KeyPoolVersioningAllowed(versioningAllowed)
+	return &keyPoolKeyPoolVersioningAllowed
+}
+
+func ValidateString(value string) error {
+	length := len(value)
+	trimmedLength := len(strings.TrimSpace(value))
+	if length == 0 {
+		return fmt.Errorf("string can't be empty")
+	} else if trimmedLength == 0 {
+		return fmt.Errorf("string can't contain all whitespace")
+	} else if trimmedLength != length {
+		return fmt.Errorf("string can't contain leading or trailing whitespace")
+	}
+	return nil
+}
--- internal/client/client_test.go
+++ internal/client/client_test.go
@@ -6,8 +6,6 @@ import (
 	cryptoutilOpenapiModel "cryptoutil/internal/openapi/model"
 	cryptoutilServer "cryptoutil/internal/server/listener"
 	"encoding/json"
-	"errors"
-	"fmt"
 	"strings"
 	"testing"
 
@@ -97,7 +95,7 @@ func RequireClientWithResponses(t *testing.T, baseUrl string) *cryptoutilOpenapi
 }
 
 func RequireCreateKeyPoolRequest(t *testing.T, name string, description string, algorithm string, provider string, exportAllowed bool, importAllowed bool, versioningAllowed bool) *cryptoutilOpenapiClient.PostKeypoolJSONRequestBody {
-	openapiCreateKeyPoolRequest, err := mapKeyPoolCreate(t, name, description, algorithm, provider, exportAllowed, importAllowed, versioningAllowed)
+	openapiCreateKeyPoolRequest, err := MapKeyPoolCreate(t, name, description, algorithm, provider, exportAllowed, importAllowed, versioningAllowed)
 	require.NotNil(t, openapiCreateKeyPoolRequest)
 	require.NoError(t, err)
 	return openapiCreateKeyPoolRequest
@@ -134,156 +132,4 @@ func RequireCreateKeyPoolResponse(t *testing.T, context context.Context, openapi
 		assert.FailNowf(t, "", "failed to create key pool, Status: %v, Message: %s", openapiCreateKeyPoolResponse.HTTPResponse.StatusCode, openapiCreateKeyPoolResponse.HTTPResponse.Status)
 	}
 	return openapiCreateKeyPoolResponse
-}
-
-func mapKeyPoolCreate(t *testing.T, name string, description string, algorithm string, provider string, exportAllowed bool, importAllowed bool, versioningAllowed bool) (*cryptoutilOpenapiClient.PostKeypoolJSONRequestBody, error) {
-	keyPoolName, err1 := mapKeyPoolName(name)
-	keyPoolDescription, err2 := mapKeyPoolDescription(description)
-	keyPoolAlgorithm, err3 := mapKeyPoolAlgorithm(algorithm)
-	keyPoolProvider, err4 := mapKeyPoolProvider(provider)
-	keyPoolKeyPoolExportAllowed := mapKeyPoolExportAllowed(exportAllowed)
-	keyPoolKeyPoolImportAllowed := mapKeyPoolImportAllowed(importAllowed)
-	keyPoolKeyPoolVersioningAllowed := mapKeyPoolVersioningAllowed(versioningAllowed)
-	if err1 != nil || err2 != nil || err3 != nil || err4 != nil {
-		t.Fatalf("failed to map key pool: %v", errors.Join(err1, err2, err3, err4))
-	}
-	return &cryptoutilOpenapiModel.KeyPoolCreate{
-		Name:              *keyPoolName,
-		Description:       *keyPoolDescription,
-		Provider:          keyPoolProvider,
-		Algorithm:         keyPoolAlgorithm,
-		ExportAllowed:     keyPoolKeyPoolExportAllowed,
-		ImportAllowed:     keyPoolKeyPoolImportAllowed,
-		VersioningAllowed: keyPoolKeyPoolVersioningAllowed,
-	}, nil
-}
-
-func mapKeyPoolName(name string) (*cryptoutilOpenapiModel.KeyPoolName, error) {
-	if err := ValidateString(name); err != nil {
-		return nil, fmt.Errorf("invalid key pool name: %w", err)
-	}
-	keyPoolName := cryptoutilOpenapiModel.KeyPoolName(name)
-	return &keyPoolName, nil
-}
-
-func ValidateString(value string) error {
-	length := len(value)
-	trimmedLength := len(strings.TrimSpace(value))
-	if length == 0 {
-		return fmt.Errorf("string can't be empty")
-	} else if trimmedLength == 0 {
-		return fmt.Errorf("string can't contain all whitespace")
-	} else if trimmedLength != length {
-		return fmt.Errorf("string can't contain leading or trailing whitespace")
-	}
-	return nil
-}
-
-func mapKeyPoolDescription(description string) (*cryptoutilOpenapiModel.KeyPoolDescription, error) {
-	if err := ValidateString(description); err != nil {
-		return nil, fmt.Errorf("invalid key pool description: %w", err)
-	}
-	keyPoolDescription := cryptoutilOpenapiModel.KeyPoolDescription(description)
-	return &keyPoolDescription, nil
-}
-
-func mapKeyPoolAlgorithm(algorithm string) (*cryptoutilOpenapiModel.KeyPoolAlgorithm, error) {
-	if err := ValidateString(algorithm); err != nil {
-		return nil, fmt.Errorf("invalid key pool algorithm: %w", err)
-	}
-	var keyPoolAlgorithm cryptoutilOpenapiModel.KeyPoolAlgorithm
-	switch algorithm {
-	case string(cryptoutilOpenapiModel.A128CBCHS256A128GCMKW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A128CBCHS256A128GCMKW
-	case string(cryptoutilOpenapiModel.A128CBCHS256A128KW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A128CBCHS256A128KW
-	case string(cryptoutilOpenapiModel.A128CBCHS256A192GCMKW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A128CBCHS256A192GCMKW
-	case string(cryptoutilOpenapiModel.A128CBCHS256A192KW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A128CBCHS256A192KW
-	case string(cryptoutilOpenapiModel.A128CBCHS256A256GCMKW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A128CBCHS256A256GCMKW
-	case string(cryptoutilOpenapiModel.A128CBCHS256A256KW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A128CBCHS256A256KW
-	case string(cryptoutilOpenapiModel.A128CBCHS256dir):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A128CBCHS256dir
-	case string(cryptoutilOpenapiModel.A128GCMA128GCMKW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A128GCMA128GCMKW
-	case string(cryptoutilOpenapiModel.A128GCMA128KW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A128GCMA128KW
-	case string(cryptoutilOpenapiModel.A128GCMA192GCMKW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A128GCMA192GCMKW
-	case string(cryptoutilOpenapiModel.A128GCMA192KW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A128GCMA192KW
-	case string(cryptoutilOpenapiModel.A128GCMA256GCMKW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A128GCMA256GCMKW
-	case string(cryptoutilOpenapiModel.A128GCMA256KW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A128GCMA256KW
-	case string(cryptoutilOpenapiModel.A128GCMdir):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A128GCMdir
-	case string(cryptoutilOpenapiModel.A192CBCHS384A192GCMKW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A192CBCHS384A192GCMKW
-	case string(cryptoutilOpenapiModel.A192CBCHS384A192KW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A192CBCHS384A192KW
-	case string(cryptoutilOpenapiModel.A192CBCHS384A256GCMKW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A192CBCHS384A256GCMKW
-	case string(cryptoutilOpenapiModel.A192CBCHS384A256KW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A192CBCHS384A256KW
-	case string(cryptoutilOpenapiModel.A192CBCHS384dir):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A192CBCHS384dir
-	case string(cryptoutilOpenapiModel.A192GCMA192GCMKW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A192GCMA192GCMKW
-	case string(cryptoutilOpenapiModel.A192GCMA192KW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A192GCMA192KW
-	case string(cryptoutilOpenapiModel.A192GCMA256GCMKW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A192GCMA256GCMKW
-	case string(cryptoutilOpenapiModel.A192GCMA256KW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A192GCMA256KW
-	case string(cryptoutilOpenapiModel.A192GCMdir):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A192GCMdir
-	case string(cryptoutilOpenapiModel.A256CBCHS512A256GCMKW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A256CBCHS512A256GCMKW
-	case string(cryptoutilOpenapiModel.A256CBCHS512A256KW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A256CBCHS512A256KW
-	case string(cryptoutilOpenapiModel.A256CBCHS512dir):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A256CBCHS512dir
-	case string(cryptoutilOpenapiModel.A256GCMA256GCMKW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A256GCMA256GCMKW
-	case string(cryptoutilOpenapiModel.A256GCMA256KW):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A256GCMA256KW
-	case string(cryptoutilOpenapiModel.A256GCMdir):
-		keyPoolAlgorithm = cryptoutilOpenapiModel.A256GCMdir
-	default:
-		return nil, fmt.Errorf("invalid key pool algorithm: %s", algorithm)
-	}
-	return &keyPoolAlgorithm, nil
-}
-
-func mapKeyPoolProvider(provider string) (*cryptoutilOpenapiModel.KeyPoolProvider, error) {
-	if err := ValidateString(provider); err != nil {
-		return nil, fmt.Errorf("invalid key pool provider: %w", err)
-	}
-	var keyPoolProvider cryptoutilOpenapiModel.KeyPoolProvider
-	switch provider {
-	case string(cryptoutilOpenapiModel.Internal):
-		keyPoolProvider = cryptoutilOpenapiModel.Internal
-	default:
-		return nil, fmt.Errorf("invalid key pool provider: %s", provider)
-	}
-	return &keyPoolProvider, nil
-}
-
-func mapKeyPoolImportAllowed(importAllowed bool) *cryptoutilOpenapiModel.KeyPoolImportAllowed {
-	keyPoolKeyPoolImportAllowed := cryptoutilOpenapiModel.KeyPoolImportAllowed(importAllowed)
-	return &keyPoolKeyPoolImportAllowed
-}
-
-func mapKeyPoolExportAllowed(exportAllowed bool) *cryptoutilOpenapiModel.KeyPoolExportAllowed {
-	keyPoolKeyPoolExportAllowed := cryptoutilOpenapiModel.KeyPoolExportAllowed(exportAllowed)
-	return &keyPoolKeyPoolExportAllowed
-}
-
-func mapKeyPoolVersioningAllowed(versioningAllowed bool) *cryptoutilOpenapiModel.KeyPoolVersioningAllowed {
-	keyPoolKeyPoolVersioningAllowed := cryptoutilOpenapiModel.KeyPoolVersioningAllowed(versioningAllowed)
-	return &keyPoolKeyPoolVersioningAllowed
 }
