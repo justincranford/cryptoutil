558767c213a6558421845f04a3ebc1342295d48a
Justin Cranford <justincranford@hotmail.com>
2025-05-05 22:40:57 -0400

    Moved ValidateCreateKeyPoolVsKeyPool

--- internal/client/client_mapper.go
+++ internal/client/client_mapper.go
@@ -200,30 +200,3 @@ func ValidateString(value string) error {
 	}
 	return nil
 }
-
-func ValidateCreateKeyPoolVsKeyPool(keyPoolCreate *cryptoutilOpenapiModel.KeyPoolCreate, keyPool *cryptoutilOpenapiModel.KeyPool) error {
-	if keyPoolCreate == nil {
-		return fmt.Errorf("key pool create is nil")
-	} else if keyPool == nil {
-		return fmt.Errorf("key pool is nil")
-	} else if keyPool.Id == nil {
-		return fmt.Errorf("key pool ID is nil")
-	} else if keyPoolCreate.Name != *keyPool.Name {
-		return fmt.Errorf("name mismatch: expected %s, got %s", keyPoolCreate.Name, *keyPool.Name)
-	} else if keyPoolCreate.Description != *keyPool.Description {
-		return fmt.Errorf("description mismatch: expected %s, got %s", keyPoolCreate.Description, *keyPool.Description)
-	} else if *keyPoolCreate.Algorithm != *keyPool.Algorithm {
-		return fmt.Errorf("algorithm mismatch: expected %s, got %s", *keyPoolCreate.Algorithm, *keyPool.Algorithm)
-	} else if *keyPoolCreate.Provider != *keyPool.Provider {
-		return fmt.Errorf("provider mismatch: expected %s, got %s", *keyPoolCreate.Provider, *keyPool.Provider)
-	} else if *keyPoolCreate.ExportAllowed != *keyPool.ExportAllowed {
-		return fmt.Errorf("exportAllowed mismatch: expected %t, got %t", *keyPoolCreate.ExportAllowed, *keyPool.ExportAllowed)
-	} else if *keyPoolCreate.ImportAllowed != *keyPool.ImportAllowed {
-		return fmt.Errorf("importAllowed mismatch: expected %t, got %t", *keyPoolCreate.ImportAllowed, *keyPool.ImportAllowed)
-	} else if *keyPoolCreate.VersioningAllowed != *keyPool.VersioningAllowed {
-		return fmt.Errorf("versioningAllowed mismatch: expected %t, got %t", *keyPoolCreate.VersioningAllowed, *keyPool.VersioningAllowed)
-	} else if cryptoutilOpenapiModel.Active != *keyPool.Status {
-		return fmt.Errorf("status mismatch: expected %s, got %s", cryptoutilOpenapiModel.Active, *keyPool.Status)
-	}
-	return nil
-}
