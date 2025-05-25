183af687d7ab90fcaac88e5533a46dfd3a3d3097
Justin Cranford <justincranford@hotmail.com>
2025-05-24 23:15:43 -0400

    Added test cov for JWS JWK in JwkGenService

--- internal/common/crypto/jose/jwk_gen_service_test.go
+++ internal/common/crypto/jose/jwk_gen_service_test.go
@@ -92,3 +92,59 @@ func Test_HappyPath_JwkGenService_Jwe_Jwk_EncryptDecryptBytes(t *testing.T) {
 		})
 	}
 }
+
+func Test_HappyPath_JwkGenService_Jws_Jwk_SignVerifyBytes(t *testing.T) {
+	for _, testCase := range happyPathJwsTestCases {
+		plaintext := fmt.Appendf(nil, "Hello world alg=%s!", testCase.alg)
+		t.Run(fmt.Sprintf("%v", testCase.alg), func(t *testing.T) {
+			t.Parallel()
+
+			jwsJwkKid, jwsJwk, encodedJwsJwk, err := testJwkGenService.GenerateJwsJwk(testCase.alg)
+			require.NoError(t, err)
+			require.NotEmpty(t, jwsJwkKid)
+			require.NotNil(t, jwsJwk)
+			require.NotEmpty(t, encodedJwsJwk)
+			log.Printf("Generated: %s", encodedJwsJwk)
+
+			var actualJwkAlg joseJwa.KeyAlgorithm
+			require.NoError(t, jwsJwk.Get(joseJwk.AlgorithmKey, &actualJwkAlg))
+			require.Equal(t, *testCase.alg, actualJwkAlg)
+
+			var actualJwkUse string
+			require.NoError(t, jwsJwk.Get(joseJwk.KeyUsageKey, &actualJwkUse))
+			require.Equal(t, "sig", actualJwkUse)
+
+			var actualJwkOps joseJwk.KeyOperationList
+			require.NoError(t, jwsJwk.Get(joseJwk.KeyOpsKey, &actualJwkOps))
+			require.Equal(t, OpsSigVer, actualJwkOps)
+
+			var actualJwkKty joseJwa.KeyType
+			require.NoError(t, jwsJwk.Get(joseJwk.KeyTypeKey, &actualJwkKty))
+			require.Equal(t, testCase.expectedType, actualJwkKty)
+
+			jwsMessage, encodedJwsMessage, err := SignBytes([]joseJwk.Key{jwsJwk}, plaintext)
+			require.NoError(t, err)
+			require.NotEmpty(t, encodedJwsMessage)
+			log.Printf("JWS Message: %s", string(encodedJwsMessage))
+
+			jwsHeaders := jwsMessage.Signatures()[0].ProtectedHeaders()
+			encodedJweHeaders, err := json.Marshal(jwsHeaders)
+			require.NoError(t, err)
+			log.Printf("JWS Message Headers: %v", string(encodedJweHeaders))
+
+			var actualJweKid string
+			require.NoError(t, jwsHeaders.Get(joseJwk.KeyIDKey, &actualJweKid))
+			require.NotEmpty(t, actualJweKid)
+			require.Equal(t, jwsJwkKid.String(), actualJweKid)
+
+			var actualJwsAlg joseJwa.KeyAlgorithm
+			require.NoError(t, jwsHeaders.Get(joseJwk.AlgorithmKey, &actualJwsAlg))
+			require.Equal(t, *testCase.alg, actualJwsAlg)
+			require.Equal(t, actualJwkAlg, actualJwsAlg)
+
+			verified, err := VerifyBytes([]joseJwk.Key{jwsJwk}, encodedJwsMessage)
+			require.NoError(t, err)
+			require.NotNil(t, verified)
+		})
+	}
+}
