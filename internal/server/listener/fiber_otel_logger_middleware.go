a15ea11bb83627e323c407e14c972c74dbc1ff26
Justin Cranford <justincranford@hotmail.com>
2025-06-01 20:59:07 -0400

    Appended error to log in fiberOtelLoggerMiddleware

--- internal/server/listener/fiber_otel_logger_middleware.go
+++ internal/server/listener/fiber_otel_logger_middleware.go
@@ -18,14 +18,18 @@ func fiberOtelLoggerMiddleware(logger *slog.Logger) fiber.Handler {
 		spanContext := span.SpanContext()
 
 		// Log request details with OpenTelemetry correlation
-		logger.Info("Responded",
+		args := []any{
 			slog.Int("status", c.Response().StatusCode()),
 			slog.Duration("duration_ms", time.Since(start)),
 			slog.String("method", c.Method()),
 			slog.String("path", c.Path()),
 			slog.String("trace_id", spanContext.TraceID().String()),
 			slog.String("span_id", spanContext.SpanID().String()),
-		)
+		}
+		if err != nil {
+			args = append(args, slog.String("error", err.Error()))
+		}
+		logger.Info("Responded", args...)
 		return err
 	}
 }
