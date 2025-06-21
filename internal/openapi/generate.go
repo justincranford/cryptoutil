e2da6c1dfcf3f0f593250a1cc676cd465089ec03
Justin Cranford <justincranford@hotmail.com>
2025-06-21 00:18:04 -0400

    Renamed config files to /openapi-gen_config_*.yaml

--- internal/openapi/generate.go
+++ internal/openapi/generate.go
@@ -1,5 +1,5 @@
 package openapi
 
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@v2.4.1-0.20250618140738-aae687ce8fe9 --config=./openapi_gen_model.yaml  ./openapi_spec_components.yaml
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@v2.4.1-0.20250618140738-aae687ce8fe9 --config=./openapi_gen_server.yaml ./openapi_spec_paths.yaml
-//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@v2.4.1-0.20250618140738-aae687ce8fe9 --config=./openapi_gen_client.yaml ./openapi_spec_paths.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@v2.4.1-0.20250618140738-aae687ce8fe9 --config=./openapi-gen_config_model.yaml  ./openapi_spec_components.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@v2.4.1-0.20250618140738-aae687ce8fe9 --config=./openapi-gen_config_server.yaml ./openapi_spec_paths.yaml
+//go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@v2.4.1-0.20250618140738-aae687ce8fe9 --config=./openapi-gen_config_client.yaml ./openapi_spec_paths.yaml
