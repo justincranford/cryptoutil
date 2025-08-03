2fb0021a286ad46b424009d2e42764a3e68f225c
Justin Cranford <justincranford@hotmail.com>
2025-08-03 17:19:43 -0400

    Added missing properties to test settings

--- internal/common/config/config_test_util.go
+++ internal/common/config/config_test_util.go
@@ -19,9 +19,6 @@ func RequireNewForTest(applicationName string) *Settings {
 		VerboseMode:              verboseMode.value.(bool),
 		LogLevel:                 logLevel.value.(string),
 		DevMode:                  devMode.value.(bool),
-		OTLP:                     otlp.value.(bool),
-		OTLPConsole:              otlpConsole.value.(bool),
-		OTLPScope:                otlpScope.value.(string),
 		BindAddress:              bindAddress.value.(string),
 		BindPort:                 uint16(currentBindPort.Add(1)),
 		ContextPath:              contextPath.value.(string),
@@ -29,6 +26,9 @@ func RequireNewForTest(applicationName string) *Settings {
 		CORSAllowedMethods:       corsAllowedMethods.value.(string),
 		CORSAllowedHeaders:       corsAllowedHeaders.value.(string),
 		CORSMaxAge:               corsMaxAge.value.(uint16),
+		CSRFTokenName:            csrfTokenName.value.(string),
+		CSRFTokenSameSite:        csrfTokenSameSite.value.(string),
+		CSRFTokenMaxAge:          csrfTokenMaxAge.value.(time.Duration),
 		IPRateLimit:              ipRateLimit.value.(uint16),
 		AllowedIPs:               allowedIps.value.(string),
 		AllowedCIDRs:             allowedCidrs.value.(string),
@@ -36,6 +36,11 @@ func RequireNewForTest(applicationName string) *Settings {
 		DatabaseURL:              databaseURL.value.(string),
 		DatabaseInitTotalTimeout: databaseInitTotalTimeout.value.(time.Duration),
 		DatabaseInitRetryWait:    databaseInitRetryWait.value.(time.Duration),
+		OTLP:                     otlp.value.(bool),
+		OTLPConsole:              otlpConsole.value.(bool),
+		OTLPScope:                otlpScope.value.(string),
+		UnsealMode:               unsealMode.value.(string),
+		UnsealFiles:              unsealFiles.value.([]string),
 	}
 	// Overrides for testing
 	settings.LogLevel = "ALL"
