66de1228ccab97cf389a98794df6f5363e12a680
Justin Cranford <justincranford@hotmail.com>
2025-08-03 17:28:18 -0400

    Moved TestMain from jwkgen_service_test.go to testmain_test.go in jose package

--- internal/common/crypto/jose/jwkgen_service_test.go
+++ internal/common/crypto/jose/jwkgen_service_test.go
@@ -1,43 +1,14 @@
 package jose
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 	"log"
-	"os"
 	"testing"
-
-	cryptoutilAppErr "cryptoutil/internal/common/apperr"
-	cryptoutilConfig "cryptoutil/internal/common/config"
-	cryptoutilTelemetry "cryptoutil/internal/common/telemetry"
 
 	joseJwk "github.com/lestrrat-go/jwx/v3/jwk"
 	"github.com/stretchr/testify/require"
 )
-
-var (
-	testSettings         = cryptoutilConfig.RequireNewForTest("jwkgen_service_test")
-	testCtx              = context.Background()
-	testTelemetryService *cryptoutilTelemetry.TelemetryService
-	testJwkGenService    *JwkGenService
-)
-
-func TestMain(m *testing.M) {
-	var rc int
-	func() {
-		testTelemetryService = cryptoutilTelemetry.RequireNewForTest(testCtx, testSettings)
-		defer testTelemetryService.Shutdown()
-
-		var err error
-		testJwkGenService, err = NewJwkGenService(testCtx, testTelemetryService)
-		cryptoutilAppErr.RequireNoError(err, "failed to initialize NewJwkGenService")
-		defer testJwkGenService.Shutdown()
-
-		rc = m.Run()
-	}()
-	os.Exit(rc)
-}
 
 func Test_HappyPath_JwkGenService_Jwe_Jwk_EncryptDecryptBytes(t *testing.T) {
 	for _, testCase := range happyPathJweTestCases {
