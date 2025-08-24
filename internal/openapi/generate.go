5f2c76818b9ab00f46481389f66495709c4bd46f
Justin Cranford <justincranford@hotmail.com>
2025-08-24 14:22:08 -0400

    Added TODO to move openapi spec docs

--- internal/openapi/generate.go
+++ internal/openapi/generate.go
@@ -1,5 +1,6 @@
 package openapi
 
+// TODO Move to golang command pattern directory
 //go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@v2.4.1-0.20250618140738-aae687ce8fe9 --config=./openapi-gen_config_model.yaml  ./openapi_spec_components.yaml
 //go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@v2.4.1-0.20250618140738-aae687ce8fe9 --config=./openapi-gen_config_server.yaml ./openapi_spec_paths.yaml
 //go:generate go run github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@v2.4.1-0.20250618140738-aae687ce8fe9 --config=./openapi-gen_config_client.yaml ./openapi_spec_paths.yaml
