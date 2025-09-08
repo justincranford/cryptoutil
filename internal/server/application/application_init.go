9e9d7a5c21d4617243d0ac96574cf2a851a06c64
Justin Cranford <justincranford@hotmail.com>
2025-09-07 20:46:33 -0400

    Encrypted TLS Server Private Key with UnsealKeysService.EncryptData

--- internal/server/application/application_init.go
+++ internal/server/application/application_init.go
@@ -8,7 +8,7 @@ import (
 	"os"
 
 	cryptoutilConfig "cryptoutil/internal/common/config"
-	"cryptoutil/internal/common/crypto/asn1"
+	cryptoutilAsn1 "cryptoutil/internal/common/crypto/asn1"
 	cryptoutilCertificate "cryptoutil/internal/common/crypto/certificate"
 	cryptoutilDateTime "cryptoutil/internal/common/util/datetime"
 )
@@ -45,37 +45,42 @@ func ServerInit(settings *cryptoutilConfig.Settings) error {
 }
 
 func generateTLSServerCertificates(serverApplicationBasic *ServerApplicationBasic, prefix string, publicTLSServerDNSNames []string, publicTLSServerIPAddresses []net.IP) error {
-	publicTLSServerSubjectsKeyPairs := serverApplicationBasic.JwkGenService.ECDSAP256KeyGenPool.GetMany(2)
-	publicTLSServerCASubjects, err := cryptoutilCertificate.CreateCASubjects(publicTLSServerSubjectsKeyPairs[1:], "TLS Server CA", 10*365*cryptoutilDateTime.Days1)
+	tlsServerSubjectsKeyPairs := serverApplicationBasic.JwkGenService.ECDSAP256KeyGenPool.GetMany(2)
+
+	tlsServerCASubjects, err := cryptoutilCertificate.CreateCASubjects(tlsServerSubjectsKeyPairs[1:], "TLS Server CA", 10*365*cryptoutilDateTime.Days1)
 	if err != nil {
 		return fmt.Errorf("failed to create TLS server CA subjects: %w", err)
 	}
-	publicTLSServerEndEntitySubject, err := cryptoutilCertificate.CreateEndEntitySubject(publicTLSServerCASubjects[0], publicTLSServerSubjectsKeyPairs[0], "TLS Server", 397*cryptoutilDateTime.Days1, publicTLSServerDNSNames, publicTLSServerIPAddresses, nil, nil, x509.KeyUsageDigitalSignature, []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth})
+
+	tlsServerEndEntitySubject, err := cryptoutilCertificate.CreateEndEntitySubject(tlsServerCASubjects[0], tlsServerSubjectsKeyPairs[0], "TLS Server", 397*cryptoutilDateTime.Days1, publicTLSServerDNSNames, publicTLSServerIPAddresses, nil, nil, x509.KeyUsageDigitalSignature, []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth})
 	if err != nil {
 		return fmt.Errorf("failed to create TLS server end entity subject: %w", err)
-	} else if publicTLSServerEndEntitySubject == nil {
-		return fmt.Errorf("publicTLSServerEndEntitySubject is nil")
 	}
 
-	// Encode Cert Chain and Private Key as PEM
-	publicTLSServerCertificateChainPEMs, err := asn1.PemEncodes(publicTLSServerEndEntitySubject.KeyMaterial.CertificateChain)
+	// Encode Certificates as PEM and write to files
+	tlsServerCertificateChainPEMs, err := cryptoutilAsn1.PemEncodes(tlsServerEndEntitySubject.KeyMaterial.CertificateChain)
 	if err != nil {
-		return fmt.Errorf("failed to encode certificate chain to PEM: %w", err)
+		return fmt.Errorf("failed to encode certificate chain as PEM: %w", err)
 	}
-	publicTLSPrivateKeyPEM, err := asn1.PemEncode(publicTLSServerEndEntitySubject.KeyMaterial.PrivateKey)
-	if err != nil {
-		return fmt.Errorf("failed to encode private key to PEM: %w", err)
-	}
-
-	// Write Cert Chain and Private Key PEM files to files
-	for i, certPEM := range publicTLSServerCertificateChainPEMs {
+	for i, certPEM := range tlsServerCertificateChainPEMs {
 		filename := fmt.Sprintf("%scertificate_%d.pem", prefix, i)
 		if err := os.WriteFile(filename, certPEM, 0600); err != nil {
-			return fmt.Errorf("failed to write public TLS server certificate chain PEM file %s: %w", filename, err)
+			return fmt.Errorf("failed to write TLS server certificate PEM file %s: %w", filename, err)
 		}
 	}
-	if err := os.WriteFile(fmt.Sprintf("%sprivate_key.pem", prefix), publicTLSPrivateKeyPEM, 0600); err != nil {
-		return fmt.Errorf("failed to write public TLS server private key PEM file: %w", err)
+
+	// Encrypt private key as PEM to write to file
+	tlsPrivateKeyPEM, err := cryptoutilAsn1.PemEncode(tlsServerEndEntitySubject.KeyMaterial.PrivateKey)
+	if err != nil {
+		return fmt.Errorf("failed to encode private key as PEM: %w", err)
+	}
+	encryptedTLSPrivateKeyPEM, err := serverApplicationBasic.UnsealKeysService.EncryptData(tlsPrivateKeyPEM)
+	if err != nil {
+		return fmt.Errorf("failed to encrypt TLS server private key PEM: %w", err)
+	}
+	err = os.WriteFile(fmt.Sprintf("%sprivate_key.pem", prefix), encryptedTLSPrivateKeyPEM, 0600)
+	if err != nil {
+		return fmt.Errorf("failed to write encrypted TLS server private key PEM file: %w", err)
 	}
 	return nil
 }
