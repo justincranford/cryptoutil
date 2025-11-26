368e590afbe45cfd7f550d3bcaaf60fa1f4c9f3d
Justin Cranford <justincranford@hotmail.com>
2025-11-25 20:07:35 -0500

    test(clientauth): add TLS client auth tests
    - TestTLSClientAuthenticator_Authenticate_Cert
    - TestTLSClientAuthenticator_Method_Cert
    - Test helpers for CA and client cert generation
    - Coverage: 47.2% -> 56.4% (+9.2pp)

---
+++ internal/identity/authz/clientauth/test_helpers_cert.go
@@ -0,0 +1,68 @@
+package clientauth
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	crand "crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"math/big"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+// Helper functions for certificate generation in tests.
+
+func createTestCAForAuth(t *testing.T) (*x509.Certificate, *ecdsa.PrivateKey) {
+	t.Helper()
+
+	key, err := ecdsa.GenerateKey(elliptic.P256(), crand.Reader)
+	require.NoError(t, err, "CA key generation should succeed")
+
+	template := &x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject: pkix.Name{
+			CommonName: "Test CA",
+		},
+		NotBefore:             time.Now().Add(-1 * time.Hour),
+		NotAfter:              time.Now().Add(24 * time.Hour),
+		KeyUsage:              x509.KeyUsageCertSign,
+		BasicConstraintsValid: true,
+		IsCA:                  true,
+	}
+
+	certDER, err := x509.CreateCertificate(crand.Reader, template, template, &key.PublicKey, key)
+	require.NoError(t, err, "CA certificate creation should succeed")
+
+	cert, err := x509.ParseCertificate(certDER)
+	require.NoError(t, err, "CA certificate parsing should succeed")
+
+	return cert, key
+}
+
+func createTestClientCertForAuth(t *testing.T, caCert *x509.Certificate, caKey *ecdsa.PrivateKey) *x509.Certificate {
+	t.Helper()
+
+	key, err := ecdsa.GenerateKey(elliptic.P256(), crand.Reader)
+	require.NoError(t, err, "Client key generation should succeed")
+
+	template := &x509.Certificate{
+		SerialNumber: big.NewInt(2),
+		Subject: pkix.Name{
+			CommonName: "Test Client",
+		},
+		NotBefore: time.Now().Add(-1 * time.Hour),
+		NotAfter:  time.Now().Add(24 * time.Hour),
+		KeyUsage:  x509.KeyUsageDigitalSignature,
+	}
+
+	certDER, err := x509.CreateCertificate(crand.Reader, template, caCert, &key.PublicKey, caKey)
+	require.NoError(t, err, "Client certificate creation should succeed")
+
+	cert, err := x509.ParseCertificate(certDER)
+	require.NoError(t, err, "Client certificate parsing should succeed")
+
+	return cert
+}
