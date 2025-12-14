6f48adb84b6d1694a81fea0b6e86adc185e4809e
Justin Cranford <justincranford@hotmail.com>
2025-12-14 04:34:34 -0500

    feat(ca): add OpenAPI client generation for E2E testing

--- api/ca/generate.go
+++ api/ca/generate.go
@@ -4,3 +4,4 @@ package ca
 
 //go:generate oapi-codegen --config=openapi-gen_config_models.yaml openapi_spec_enrollment.yaml
 //go:generate oapi-codegen --config=openapi-gen_config_server.yaml openapi_spec_enrollment.yaml
+//go:generate oapi-codegen --config=openapi-gen_config_client.yaml openapi_spec_enrollment.yaml
