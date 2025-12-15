9f5e0a0f70866d5fe5c98e0b64f9a74676e33ddf
Justin Cranford <justincranford@hotmail.com>
2025-12-15 00:02:18 -0500

    fix: use crypto/rand instead of math/rand for probability sampling
    - Updated SkipByProbability to use crypto/rand.Read() for cryptographically secure randomness
    - Added bytesPerUint64 constant to avoid magic number linting issue
    - Implemented proper uint64 construction from random bytes using loop
    - Normalized random value to [0,1) range by dividing by math.MaxUint64
    - Complies with security requirement: 'ALWAYS use crypto/rand, never math/rand'

--- internal/shared/util/probability.go
+++ internal/shared/util/probability.go
@@ -5,14 +5,37 @@
 package util
 
 import (
-	"math/rand"
+	"crypto/rand"
+	"math"
 	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	// bytesPerUint64 is the number of bytes in a uint64.
+	bytesPerUint64 = 8
 )
 
 // SkipByProbability skips the test based on the given probability.
 // prob should be between 0.0 and 1.0, where 1.0 means always run, 0.0 means never run.
 func SkipByProbability(t *testing.T, prob float64) {
-	if rand.Float64() > prob { //nolint:gosec // math/rand is acceptable for test probability sampling
+	require.GreaterOrEqual(t, prob, 0.0)
+	require.LessOrEqual(t, prob, 1.0)
+
+	var b [bytesPerUint64]byte
+
+	_, err := rand.Read(b[:])
+	require.NoError(t, err)
+
+	randomUint64 := uint64(0)
+	for i, v := range b {
+		randomUint64 |= uint64(v) << (i * bytesPerUint64)
+	}
+
+	normalized := float64(randomUint64) / float64(math.MaxUint64)
+
+	if normalized > prob {
 		t.Skip("Skipped by probability sampling")
 	}
 }
