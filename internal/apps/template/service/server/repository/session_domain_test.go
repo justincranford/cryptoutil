e441621585e3c5936e525a40a6afcbd807188c1a
Justin Cranford <justincranford@hotmail.com>
2026-01-13 07:30:55 -0500

    style: fix trailing whitespace in session_domain_test.go

--- internal/apps/template/service/server/repository/session_domain_test.go
+++ internal/apps/template/service/server/repository/session_domain_test.go
@@ -61,7 +61,7 @@ func TestSession_IsExpired(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-			
+
 			browserSession := &repository.BrowserSession{
 				Session: repository.Session{Expiration: tt.expiration},
 			}
